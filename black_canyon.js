b16472ecd18b6512979f86aca0846fd015692d8b
Seth-hulsmeyer <seth.hulsmeyer@hotmail.com>
2020-11-03 22:02:28 -0700

    added mesa verde functionality

--- black_canyon.js
+++ black_canyon.js
@@ -1,25 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll(".slider");
-});
-// Or with jQuery
 $(document).ready(function () {
-  $(".slider").slider();
   $(".button-collapse").sideNav();
+
+  // fetch for Parks API data
+  fetch(
+    "https://developer.nps.gov/api/v1/parks?parkCode=meve&api_key=hRggEhs9bPMZKvgi1LwlI8PflCnBJoFdBnLBCJlP"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      // Park Address
+      $("#mvAddress").append(
+        " " +
+          data.data[0].addresses[0].line1 +
+          ", " +
+          data.data[0].addresses[0].city +
+          ", " +
+          data.data[0].addresses[0].stateCode +
+          " " +
+          data.data[0].addresses[0].postalCode
+      );
+      //Park Price
+      $("#mvCost").append(
+        " $" +
+          data.data[0].entranceFees[5].cost +
+          " - $" +
+          data.data[0].entranceFees[0].cost
+      );
+
+      //Park schedule updates
+      $("#mvHours").append(" " + data.data[0].operatingHours[0].description);
+
+      //Park Activities
+      for (var i = 0; i < 5; i++) {
+        var activity = Math.floor(
+          Math.random() * data.data[0].activities.length
+        );
+        $("#mvActivities").append(
+          "<li>" + "- " + data.data[0].activities[activity].name + "</li>"
+        );
+      }
+    });
+
+  var userComment = [];
+  $("");
 });
-
-// fetch for Park API data
-fetch(
-  "https://developer.nps.gov/api/v1/parks?stateCode=co&api_key=hRggEhs9bPMZKvgi1LwlI8PflCnBJoFdBnLBCJlP"
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    console.log(data.data[0].states);
-
-    $("#main-img-bc").attr("src", data.data[1].images[1].url);
-    $("#main-img-sd").attr("src", data.data[7].images[0].url);
-    $("#main-img-mv").attr("src", data.data[9].images[2].url);
-    $("#main-img-rm").attr("src", data.data[12].images[0].url);
-  });
