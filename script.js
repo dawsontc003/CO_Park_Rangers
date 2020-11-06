3a14756e54cb540440a200a7e0b8057cd7a5fcca
Thomas Dawson <dawsontc003@gmail.com>
2020-11-05 23:06:28 -0700

    Second API added from Unsplashed images

--- script.js
+++ script.js
@@ -18,8 +18,34 @@ fetch(
     console.log(data);
     console.log(data.data[0].states);
 
-    $("#main-img-bc").attr("src", data.data[1].images[1].url);
     $("#main-img-sd").attr("src", data.data[7].images[0].url);
-    $("#main-img-mv").attr("src", data.data[9].images[2].url);
-    $("#main-img-rm").attr("src", data.data[12].images[0].url);
+  });
+
+fetch(
+  "https://api.unsplash.com/search/photos?page=1&query=landscape&client_id=jrk9GXC5TeN0m9Y_9-R_CqS0848U9z9wZR0OgeU94GM"
+)
+  .then(function (response2) {
+    return response2.json();
+  })
+  .then(function (data2) {
+    $("#main-img-bc").attr(
+      "src",
+      data2.results[1].tags[2].source.cover_photo.urls.small
+    );
+    console.log(data2.results[9].tags[1].source.cover_photo.urls.small);
+    $("#main-img-sd").attr(
+      "src",
+      data2.results[1].tags[2].source.cover_photo.urls.small
+    );
+    console.log(data2.results[1].tags[2].source.cover_photo.urls.small);
+    $("#main-img-mv").attr(
+      "src",
+      data2.results[2].tags[2].source.cover_photo.urls.small
+    );
+    console.log(data2.results[2].tags[2].source.cover_photo.urls.small);
+    $("#main-img-rm").attr(
+      "src",
+      data2.results[7].tags[2].source.cover_photo.urls.regular
+    );
+    console.log(data2.results[7].tags[2].source.cover_photo.urls.regular);
   });
